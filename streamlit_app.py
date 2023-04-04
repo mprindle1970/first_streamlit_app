7ae89027a5567f41bb9e20a94d3fafff81b47761
mprindle1970 <122229392+mprindle1970@users.noreply.github.com>
2023-04-04 18:29:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 
-streamlit.title('My Mom/' New Healty Diner')
+streamlit.title('My Mom New Healty Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
